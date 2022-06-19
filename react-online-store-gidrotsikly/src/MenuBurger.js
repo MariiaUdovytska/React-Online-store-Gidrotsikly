392292f06e9b0d21a6c36776102d5d75c3479a82
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-06-19 22:02:49 +0300

    initial styles for large size menu

--- react-online-store-gidrotsikly/src/MenuBurger.js
+++ react-online-store-gidrotsikly/src/MenuBurger.js
@@ -37,24 +37,24 @@ class MenuBurger extends React.Component {
 		if(sizeWidth === true){
 			return(
 				<div className='nav-bar'>
-					<Navbar collapseOnSelect expand={true} variant="light">
+					<Navbar collapseOnSelect expand={true}>
 						<Container>
 							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 							<Navbar.Collapse id="responsive-navbar-nav">
-								<Nav>
-									<Nav.Link href="#features">Магазины</Nav.Link>
-									<Nav.Link href="#pricing">Акции</Nav.Link>
-									<Nav.Link href="#pricing">Доставка и оплата</Nav.Link>
+								<Nav className='nav-bar__items-left'>
+									<Nav.Link className='nav-bar__items-a' href="#">Магазины</Nav.Link>
+									<Nav.Link className='nav-bar__items-a' href="#">Акции</Nav.Link>
+									<Nav.Link className='nav-bar__items-a' href="#">Доставка и оплата</Nav.Link>
 								</Nav>
-								<Navbar.Brand href="#home"><img src={logo} alt="logo" style={{width: '80px', height: '76px'}} /></Navbar.Brand>
-								<Nav className="justify-content-end">
-									<Nav.Link href="#deets">Город,  ул. Адрес  25</Nav.Link>
+								<Navbar.Brand className='nav-bar__items-brand' href="#home"><img src={logo} alt="logo"/></Navbar.Brand>
+								<Nav className='nav-bar__items-adress'>
+									<Nav.Link className='nav-bar__items-adress-a' href="#"><i class="bi bi-geo-alt" style={{ display: 'inline-flex', fontSize: '30px', color: '#2F3035'}}></i>Город,  ул. Адрес  25</Nav.Link>
 								</Nav>
 							</Navbar.Collapse>
-							<Nav className="justify-content-end">
-								<Nav.Link href="#features" className='no-hover'> <i className="bi bi-heart" style={{ display: 'inline-flex', fontSize: '20px', color: '#2F3035'}}></i></Nav.Link>
-								<Nav.Link href="#features" className='no-hover'><i className="bi bi-person-fill" style={{ display: 'inline-flex', fontSize: '22px', color: '#2F3035'}}></i></Nav.Link>
-								<Nav.Link href="#features" className='no-hover'><i className="bi bi-cart2" style={{ display: 'inline-flex', fontSize: '22px', color: '#2F3035'}}></i></Nav.Link>
+							<Nav className="nav-bar__items-right">
+								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'> <i className="bi bi-heart" style={{ display: 'inline-flex', fontSize: '20px', color: '#2F3035'}}></i></Nav.Link>
+								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'><i className="bi bi-person-fill" style={{ display: 'inline-flex', fontSize: '24px', color: '#2F3035'}}></i></Nav.Link>
+								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'><i className="bi bi-cart2" style={{ display: 'inline-flex', fontSize: '22px', color: '#2F3035'}}></i></Nav.Link>
 							</Nav>
 						</Container>
 					</Navbar>
--- react-online-store-gidrotsikly/src/index.js
+++ react-online-store-gidrotsikly/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
