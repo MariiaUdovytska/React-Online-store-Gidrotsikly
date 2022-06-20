7e587d169cf1a62e33a81920ad6aa66ed3276527
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-06-20 10:50:18 +0300

    add line products in large menu conponent MenuBurger

--- react-online-store-gidrotsikly/src/MenuBurger.js
+++ react-online-store-gidrotsikly/src/MenuBurger.js
@@ -48,16 +48,28 @@ class MenuBurger extends React.Component {
 								</Nav>
 								<Navbar.Brand className='nav-bar__items-brand' href="#home"><img src={logo} alt="logo"/></Navbar.Brand>
 								<Nav className='nav-bar__items-adress'>
-									<Nav.Link className='nav-bar__items-adress-a' href="#"><i class="bi bi-geo-alt" style={{ display: 'inline-flex', fontSize: '30px', color: '#2F3035'}}></i>Город,  ул. Адрес  25</Nav.Link>
+									<Nav.Link className='nav-bar__items-adress-a' href="#"><i className="bi bi-geo-alt" style={{ display: 'inline-flex', fontSize: '30px', color: '#2F3035'}}></i>Город,  ул. Адрес  25</Nav.Link>
 								</Nav>
 							</Navbar.Collapse>
 							<Nav className="nav-bar__items-right">
-								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'> <i className="bi bi-heart" style={{ display: 'inline-flex', fontSize: '20px', color: '#2F3035'}}></i></Nav.Link>
-								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'><i className="bi bi-person-fill" style={{ display: 'inline-flex', fontSize: '24px', color: '#2F3035'}}></i></Nav.Link>
-								<Nav.Link href="#" className='no-hover nav-bar__items-right-a'><i className="bi bi-cart2" style={{ display: 'inline-flex', fontSize: '22px', color: '#2F3035'}}></i></Nav.Link>
+								<Nav.Link href="#" className='nav-bar__items-right-a'> <i className="bi bi-heart" style={{ display: 'inline-flex', fontSize: '20px', color: '#2F3035'}}></i></Nav.Link>
+								<Nav.Link href="#" className='nav-bar__items-right-a'><i className="bi bi-person-fill" style={{ display: 'inline-flex', fontSize: '24px', color: '#2F3035'}}></i></Nav.Link>
+								<Nav.Link href="#" className='nav-bar__items-right-a'><i className="bi bi-cart2" style={{ display: 'inline-flex', fontSize: '22px', color: '#2F3035'}}></i></Nav.Link>
 							</Nav>
 						</Container>
 					</Navbar>
+					<div className='nav-bar__navproducts'>
+						<ul className='nav-bar__navproducts-item'>
+							<li className='nav-bar__navproducts-items'>Квадроциклы</li>
+							<li className='nav-bar__navproducts-items'>Катера</li>
+							<li className='nav-bar__navproducts-items nav-bar__navproducts-items-active'>Гидроциклы</li>
+							<li className='nav-bar__navproducts-items'>Лодки</li>
+							<li className='nav-bar__navproducts-items'>Вездеходы</li>
+							<li className='nav-bar__navproducts-items'>Снегоходы</li>
+							<li className='nav-bar__navproducts-items'>Двигатели</li>
+							<li className='nav-bar__navproducts-items'>Запчасти</li>
+						</ul>
+					</div>
 			</div>
 			)
 		} else {
