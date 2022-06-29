d1c70cbd917018bc04ba45a4ea19b483efd793d6
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-06-29 21:43:03 +0300

    added a switch for different inputs depending on the selected item

--- react-online-store-gidrotsikly/src/Search.js
+++ react-online-store-gidrotsikly/src/Search.js
@@ -4,19 +4,34 @@ import './css/search.css';
 class Search extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state ={move:0};
+	}
+
+	clickLabel = (i) =>{
+		this.setState(
+			{move:i}
+		)
 	}
 
 	render(){
+		let inputType = '';
+		switch (this.state.move) {
+			case 0: inputType='number';	break;
+			case 1: inputType='text';	break;
+			case 2: inputType='text';	break;
+			default:
+				break;
+		}
 		return(
 			<section className='search containerM'>
 				<div className='search__body'>
 					<ul className='search__body-item'>
-						<li className='search__body-items'>Поиск по номеру</li>
-						<li className='search__body-items'>Поиск по марке</li>
-						<li className='search__body-items'>Поиск по названию товара</li>
+						<li className={((this.state.move===0)?'search__body-items-active':'search__body-items')} onClick={()=>{this.clickLabel(0)}}>Поиск по номеру</li>
+						<li className={((this.state.move===1)?'search__body-items-active':'search__body-items')} onClick={()=>{this.clickLabel(1)}}>Поиск по марке</li>
+						<li className={((this.state.move===2)?'search__body-items-active':'search__body-items')} onClick={()=>{this.clickLabel(2)}}>Поиск по названию товара</li>
 					</ul>
 					<div className='search__body-inputbtn'>
-						<input className='search__body-input' type='number' placeholder='Введите...' aria-label='Number'/>
+						<input className='search__body-input' type={inputType} placeholder='Введите...' aria-label='Number'/>
 						<button className='search__body-btn' type='submit'>искать</button>
 					</div>
 				</div>
