dc0c6c0cef008386dfe532d435ddc80790ec763a
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-07-01 17:02:29 +0300

    add styles for card

--- react-online-store-gidrotsikly/src/InternalCarousel.js
+++ react-online-store-gidrotsikly/src/InternalCarousel.js
@@ -14,7 +14,14 @@ class InternalCarousel extends React.Component {
 			<div className='internal-carousel'>
 				<div className='internal-carousel__body'>
 					<ul className='internal-carousel__body-items'>
-						<li className='internal-carousel__body-card'><ProductCard/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/rukzak.png")} nameProduct={'Водонепроницаемый Рюкзак'} priceProduct={'400 $'}/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/lifejacket.png")} nameProduct={'Спасательный жилет BRP Mens Airflow PFD'} priceProduct={'320 $'}/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/audiopremiumsustem.png")} nameProduct={'BRP Audio-Premium System'} priceProduct={'1 000 $'}/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/rescueequipment.png")} nameProduct={'Спасательное снаряжение'} /></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/audioportablesystem.png")} nameProduct={'BRP Audio-портативная система'}/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/garminechomapplus.png")} nameProduct={'Garmin Echomap Plus 62cv'} priceProduct={'920 $'}/></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/tmkey.png")} nameProduct={'RF D.E.S.S.TM Key'} /></li>
+						<li className='internal-carousel__body-card' ><ProductCard imageProduct={require("./img/products/mensuit.png")} nameProduct={'Мужской костюм 3мм'} priceProduct={'100 $'}/></li>
 					</ul>
 				</div>
 			</div>
--- react-online-store-gidrotsikly/src/ProductCard.js
+++ react-online-store-gidrotsikly/src/ProductCard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './css/productCard.css';
 
+
+// const Basket = visibility.div`
+// 	visibility: ${ props => props.visibility?'white':'black'};
+// 	background: ${ props => props.positive?'green':'red' };
+// `;
+
 class ProductCard extends React.Component {
 	constructor(props) {
 		super(props);
@@ -8,11 +14,33 @@ class ProductCard extends React.Component {
 	}
 
 	render(){
-		
+		let showBusket = this.props.priceProduct !== undefined;
 		return(
 			<div className='card'>
 				<div className='card__body'>
-				
+					<div className='card__body-up'>
+						<div className='card__body-up-sale'><span>Sale</span></div>
+						<div className='card__body-up-like'><i className='bi bi-heart' style={{fontSize:'20px'}}></i></div>
+					</div>
+					<div className='card__body-imgprice'>
+						<div className='card__body-imgprice-img'>
+							<figure>
+								<img src={this.props.imageProduct} alt='imageProduct'></img>
+								<figcaption><h3>{this.props.nameProduct}</h3></figcaption>
+							</figure>
+						</div>
+						{showBusket === true
+						?<div>
+							<div className='card__body-imgprice-price'><span>{this.props.priceProduct}</span></div>
+							<div className='card__body-imgprice-basket'><i className="bi bi-cart2" style={{fontSize:'23px'}}></i></div>
+						</div>
+						:<div className='card__body-imgprice-instock'>
+							<span className='card__body-imgprice-havnot'>нет в наличии</span>
+							<a href='#' className='card__body-imgprice-reportadmission'>Сообщить о поступлении</a>
+						</div>
+						}
+						
+					</div>
 				</div>
 			</div>
 		)
