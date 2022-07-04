6f69a8f9cb9b9631b3d52a37c89880b58a7425d1
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-07-04 15:16:24 +0300

    moved the ExternalCarousel to a separate component and added props title and move

--- react-online-store-gidrotsikly/src/PopularGoods.js
+++ react-online-store-gidrotsikly/src/PopularGoods.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import './css/popularGoods.css';
-import InternalCarousel from './InternalCarousel';
-import stockCards from './data/stockCards.json';
+import ExternalCarousel from './ExternalCarousel';
 
 class PopularGoods extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state ={move:0};
+		
 	}
 
-	clickLabel = (i) =>{
-		this.setState(
-			{move:i}
-		)
-	}
 	render(){
-		let dataCard = stockCards;
 		return(
 			<section className='popular-goods containerM'>
-				<div className='popular-goods__body'>
-					<h2 className='popular-goods__body-title'>Популярные товары</h2>
-					<div className='popular-goods__body-carousel'>
-						<span className={((this.state.move===0)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(0)}}>запчасти</span>
-						<span className={((this.state.move===1)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(1)}}>моторы</span>
-						<span className={((this.state.move===2)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(2)}}>шины </span>
-						<span className={((this.state.move===3)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(3)}}>электроника</span>
-						<span className={((this.state.move===4)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(4)}}>инструменты</span>
-						<span className={((this.state.move===5)?'popular-goods__body-carousel-items-active':'popular-goods__body-carousel-items')} onClick={()=>{this.clickLabel(5)}}>аксессуары </span>
-					</div>
-					<div className='popular-goods__body-content'>
-						<div className='popular-goods__body-content-items'><InternalCarousel data={dataCard[this.state.move]}/></div>
-					</div>
-				</div>
+				<ExternalCarousel title={'Популярные товары'} move={0}/>
 			</section>
 		)
 	}
