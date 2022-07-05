d36287986171ac88fec4fc06fd36ec40d9e192d3
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-07-05 12:50:20 +0300

    add styles media for component BannerSale

--- react-online-store-gidrotsikly/src/BannerSale.js
+++ react-online-store-gidrotsikly/src/BannerSale.js
@@ -21,6 +21,14 @@ class BannerSale extends React.Component {
 					<h2 className='banner-sale__body-text'>CКИДКИ <br/> на все запчасти <br/> до 70%</h2>
 					<button className='banner-sale__body-btn' type='submit'>ПОСМОТЕТЬ&nbsp;ВСЕ</button>
 				</div>
+				<div className='banner-sale__body-phone'>
+					<h2 className='banner-sale__body-phon-text'>CКИДКИ на все запчасти <br/> до 70%</h2>
+					<div className='banner-sale__body-phon-imgswbtn'>
+						<img src={quadroLeft} alt='quadro-phon-left'></img>
+						<button className='banner-sale__body-phon-btn' type='submit'>ПОСМОТЕТЬ&nbsp;ВСЕ</button>
+						<img src={quadroRight} alt='quadro-phon-right'></img>
+					</div>
+				</div>
 			</div>
 		)
 	}
