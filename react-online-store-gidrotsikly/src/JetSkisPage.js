9b90773c864b8957299bcf5eafac4e857935144d
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-07-11 13:09:39 +0300

    add component SearchByCategory & inside commonParameters

--- react-online-store-gidrotsikly/src/JetSkisPage.js
+++ react-online-store-gidrotsikly/src/JetSkisPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/jetSkisPage.css';
+import SearchByCategory from './SearchByCategory';
 import SitePath from './SitePath';
 
 function JetSkisPage() {
 	return (
 		<main className='jet-skis-page'>
 			<SitePath/>
+			<SearchByCategory/>
 		</main>
 	);
 }
