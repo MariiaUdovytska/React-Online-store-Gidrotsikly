dda449b05a0992b3e35288e6d7c73b518cb47475
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-07-12 18:34:25 +0300

    add component AdvancedOptions and event click for main two params

--- react-online-store-gidrotsikly/src/AdvancedOptions.js
+++ react-online-store-gidrotsikly/src/AdvancedOptions.js
@@ -2,14 +2,35 @@ import React from 'react';
 import './css/advancedOptions.css';
 
 
-function AdvancedOptions() {
-	return (
-		<div className='advanced-options'>
-			<div className='advanced-options__body'>
-				<div className='advanced-options__body'></div>
+class AdvancedOptions extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state ={move:0};
+	}
+
+	clickLabel = (i) =>{
+		this.setState(
+			{move:i}
+		)
+	}
+
+	render(){
+		let value = `translateX(${-100*this.state.move}%)`;
+		return (
+			<div className='advanced-options'>
+				<div className='advanced-options__body'>
+					<div className='advanced-options__body-name'>
+						<span className={((this.state.move===0)?' advanced-options__body-name-active':'advanced-options__body-name-noactive')} onClick={()=>{this.clickLabel(0)}}>Параметры</span>
+						<span className={((this.state.move===1)?' advanced-options__body-name-active':'advanced-options__body-name-noactive')} onClick={()=>{this.clickLabel(1)}}>по&nbsp;марке</span>
+					</div>
+					<div className='advanced-options__body-items'>
+						<div className='advanced-options__body-item' style={{transform: value}}>test 1</div>
+						<div className='advanced-options__body-item' style={{transform: value}}>test 2222</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default AdvancedOptions;
--- react-online-store-gidrotsikly/src/BasicSearchOptions.js
+++ react-online-store-gidrotsikly/src/BasicSearchOptions.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import AdvancedOptions from './AdvancedOptions';
 import './css/basicSearchOptions.css';
 
 
 function BasicSearchOptions() {
 	return (
-		<section className='basic-search-options'>
+		<div className='basic-search-options'>
 			<div className='basic-search-options__body'>
-				<div className='basic-search-options__body-options'> test</div>
+				<div className='basic-search-options__body-options'><AdvancedOptions/></div>
 				<div className='basic-search-options__body-products'>test test</div>
 			</div>
-		</section>
+		</div>
 	);
 }
 
